9f4fe8fdd435f7a190c3497d78698aeaf454a647
lalabonifacio <85964193+lalabonifacio@users.noreply.github.com>
2021-06-15 16:47:47 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 700,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 5000
+});
